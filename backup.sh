623d368f5a5d250f474a363bad92ce2bf46d1eea
Simon Strobel <simon@codebrew.de>
2019-04-06 12:26:27 +0200

    [KMT] added backup strategy

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 _now=$(date +"%m_%d_%Y")
 _file="/home/istrobelapps/backup/db_$_now.aof"
-cp /home/istrobelapps/komunat/data/appendonly.aof $_file
+cp /home/istrobelapps/kommunat/data/appendonly.aof $_file
