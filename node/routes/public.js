573927e1330aed4215436f6b12b823b0577d6cb7
Simon Strobel <simon.strobel@web.de>
2019-04-06 09:38:05 +0000

    Fixed minor issues

--- node/routes/public.js
+++ node/routes/public.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Home page
 router.get("/", (req, res) => {
-  res.writeHead(301, { "Location": "https://komunat.de/dashboard" });
+    res.redirect('https://komunat.de/dashboard');
 });
 
 module.exports = router;
