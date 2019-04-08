ecb62d602e32411e1fabe1bc3adb6f245dbb1a6b
Simon Strobel <simon@codebrew.de>
2019-04-08 23:20:02 +0200

    asd

--- node/routes/profile.js
+++ node/routes/profile.js
@@ -19,8 +19,12 @@ const storage = multer.diskStorage({
   },
   filename: function(req,file,cb) {
     const s = req.sanitize(req.userinfo.sub);
+    var extname = path.extname(file.filename)
+    console.log(extname);
     if(file.mimetype === "image/jpeg") {
       cb(null,s + ".jpeg");
+    }else if (file.mimetype === "image/jpg") {
+      cb(null,s + ".jpg");
     }else if (file.mimetype === "image/png") {
       cb(null,s + ".png");
     }
