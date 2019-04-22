773651d57bd872d4a99462352aabe05fb4e65b7c
Simon Strobel <simon@codebrew.de>
2019-04-22 10:32:11 +0200

    [KMT] WIP

--- node/routes/result.js
+++ node/routes/result.js
@@ -5,9 +5,17 @@ const router = express.Router();
 const redis = require("redis")
 const request = require('request');
 const fetch = require('node-fetch');
-var db = require("../db/db_accessor")
+var db = require("../db/db_accessor");
+var static = require("../db/static");
 const expressSanitizer = require('express-sanitizer');
 const rateLimit = require("express-rate-limit");
+var nodemailer = require('nodemailer');
+var ageCalculator = require('age-calculator');
+let {AgeFromDateString, AgeFromDate} = require('age-calculator');
+const PDFDocument = require('pdfkit');
+var fs = require('fs');
+const path = require('path');
+
 
 const apiLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 1 hour window
@@ -16,47 +24,178 @@ const apiLimiter = rateLimit({
     "Too many requests from this IP, please try again later"
 });
 
+var transporter = nodemailer.createTransport({
+  host:"mx00.kundenserver.de",
+  port:25,
+  auth: {
+    user: 'Komunat@unserezukunft.org',
+    pass: 'Kanzler2017'
+  }
+});
+
 // Post result
 router.post('/',jsonParser,apiLimiter, (req, res) => {
 
-     const s = req.sanitize(req.userinfo.sub);
-
     // Store Results
+
       pushResultPromise = db.rpushResult(req.body);
       pushResultPromise.then(function updateData(index) {
         console.log("Pushed results")
       })
       // No candidate, get matches
-      candidatesPromise = db.getCandidates();
+      candidatesPromise = static.getCandidates();
       candidatesPromise.then(function cb(cdts) {
-        var matches = [];
-        cdts.forEach(function (reply, i) {
-            if (isCValid(reply)) {
-              var r = {}
-              try {
-                  r = JSON.parse(reply);
-              } catch(e) {
-                  console.log(e); // error in the above string (in this case, yes)!
-                  return;
-              }
+
+      var matches = [];
+        cdts.forEach(function (r, i) {
+            if (r.values.length != 0 && r.contents.length != 0 && r.metadata.uuid !== undefined && r.candidate.name !== undefined && r.candidate.birthdate !== undefined && r.candidate.list !== undefined && r.candidate.motto !== undefined) {
+              //console.log(r)
               var dVal = getDistance(req.body.values,r.values)
               var iVal = getDistance(req.body.contents,r.contents)
               var totalDistance = dVal+iVal;
+  
               var res = {
                 distance: totalDistance/144*100,
-                uuid: s,
+                show_distance: Math.round(totalDistance/144*100),
+                uuid: r.metadata.uuid,
                 name: r.candidate.name,
-                birthdate: r.candidate.birthdate,
+                birthdate: getAge(r.candidate.birthdate),
                 list: r.candidate.list,
                 list_number: r.candidate.list_number,
-                district: r.candidate.district
+                district: r.candidate.district,
+                motto: r.candidate.motto,
+                values: r.values,
+                contents: r.contents
               }
               matches.push(res);
+            }
+        
+        });
+
+        matches.sort(function (a, b) {
+          a.distance = a.distance
+          b.distance = b.distance
+          if (a.distance < b.distance) {
+            return 1;
           }
-        }); 
-        res.send(JSON.stringify(matches));
-    });
+          if (a.distance > b.distance) {
+            return -1;
+          }
+          return 0;
+        });
+
+        var splicer = req.body.page * 30;
+        var page = matches.slice(splicer-30,splicer);
+        console.log("Page: " + req.body.page);
+        page = sortByDistance(page);
+        console.log("Sending data") 
+        console.log(page.length)
+        res.send(JSON.stringify(page));
+      });
+      
+  }),
+
+router.post("/share",jsonParser,apiLimiter, (req, res) => {
+
+  var receiver = req.body.receiver;
+  var team =  req.body.team;
+  var result =  req.body.result;
+
+  team.sort(function (a, b) {
+    a.distance = a.distance
+    b.distance = b.distance
+    if (a.distance < b.distance) {
+      return 1;
+    }
+    if (a.distance > b.distance) {
+      return -1;
+    }
+    return 0;
+  });
+
+  const doc = new PDFDocument;
+  var v = path.join(__dirname, "/file.pdf");
+
+  var logo = path.join(__dirname,"../assets/logo.png");
+
+  doc.pipe(fs.createWriteStream(v));
+
+  doc.addPage()
+    doc.image(logo, {
+      fit: [250, 50],
+      align: 'center',
+      valign: 'center'
+   });
+   doc.fontSize(14).text("Glückwunsch, dein Team ist hier detailliert aufgelistet. Deine eigenen Werte und Aufgaben jedoch vorab:", 75, 150);
+
+   doc.fontSize(25).text("Werte:", 75, 200);
+   for(var i = 1; i < result.values.length-1; i++) {
+    doc.fontSize(12).text("Platz: " +i+ "  " + result.values[i].name, 75, 220+i*20);  
+   }
+
+   doc.addPage()
+    doc.image(logo, {
+      fit: [250, 50],
+      align: 'center',
+      valign: 'center'
+   });
+   doc.fontSize(25).text("Aufgaben:", 75, 150);
+
+   for(var i = 1; i < result.contents.length-1; i++) {
+    doc.fontSize(12).text("Platz: " +i+ "  " + result.contents[i].name, 75, 180+i*20);  
+   }
+
+
+  for(var i = 0; i < team.length; i++) {
+    doc.addPage()
+    doc.image(logo, {
+      fit: [250, 50],
+      align: 'center',
+      valign: 'center'
+   });
+    doc.fontSize(25).text(team[i].name, 75, 150);
+    doc.fontSize(12).text("Matching: " + team[i].distance, 75, 180);
+    doc.fontSize(12).text("Liste: " + team[i].list, 75, 200);
+    doc.fontSize(12).text("Listenplatz: " + team[i].list_number, 75, 220);
+    doc.fontSize(12).text("Wahlbezirk: " + team[i].district, 75, 240);  
+
+    doc.fontSize(25).text("Werte:", 75, 280);
+
+    for (var x = 1; x < team[i].values.length-1; x++) {
+      doc.fontSize(12).text("Platz: " +x+ "  " + team[i].values[x].name, 75, 290+x*20);  
+    }
+    doc.addPage()
+    doc.image(logo, {
+      fit: [250, 50],
+      align: 'center',
+      valign: 'center'
+   });
+    doc.fontSize(25).text("Aufgaben:", 75, 150);
+
+    for (var y = 1; y < team[i].contents.length-1; y++) {
+      doc.fontSize(12).text("Platz: " +y+ "  " + team[i].contents[y].name, 75, 160+y*20);  
+    }
+  }
+  doc.end();
+ /*
+  console.log("Sending email")
+  var mailOptions = {
+    from: 'Komunat@unserezukunft.org',
+    to: receiver,
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log("error ")
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
   })
+  */
+})
 
 function getDistance(x,y) {
     // To get the distance, we have to sort the array by the id's
@@ -88,6 +227,32 @@ function sortById(arr) {
     });
   return arr
 }
+
+function sortByDistance(array) {
+  return array.sort(function(a, b) {
+      var x = a.distance; var y = b.distance;
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+}
+function getAge(dateString) {
+	
+  var splitted = dateString.split(".");
+  var d = splitted[0];
+  var m = splitted[1];
+  var y = splitted[2];
+  
+  dateString = y+"/"+m+"/"+d;
+
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
+console.log('age: ' + getAge("27.12.1993"));
 
 function isCValid(r) {
   j = {}
