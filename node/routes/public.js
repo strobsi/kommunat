c8445b0bc0755ad3c88873031c69c2fb2233357a
Simon Strobel <simon@codebrew.de>
2019-04-26 00:50:02 +0200

    asd

--- node/routes/public.js
+++ node/routes/public.js
@@ -6,7 +6,13 @@ const router = express.Router();
 
 // Home page
 router.get("/", (req, res) => {
-    res.redirect('https://komunat.de/dashboard');
+    var ua = req.header('user-agent');
+    // Check the user-agent string to identyfy the device. 
+    if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+        res.redirect('https://komunat.de/app/');
+    } else {
+        res.redirect('https://komunat.de/dashboard');
+    }
 });
 
 module.exports = router;
