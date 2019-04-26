aeb94a6e71d058cfce6987d9e4d6bb9ee5ccc1d5
Simon Strobel <simon@codebrew.de>
2019-04-26 04:38:04 +0200

    [KMT] fixed css

--- node/assets/js/main.js
+++ node/assets/js/main.js
@@ -1,25 +1,4 @@
 
 $( document ).ready(function() {
 
-	infiniteChange();
 });
-
-
-function infiniteChange() {
-	var i = 0, pictures = 7;
-	function f() {
-		i++; 
-		$('#bg').css("background-image", "url(img/00"+i+".jpg)");
-		if( i < pictures ){
-			setTimeout( f, 10000 );
-		}
-		else {
-			infiniteChange();
-		}
-	}
-	f();
-}
-
-function sleep (time) {
-	return new Promise((resolve) => setTimeout(resolve, time));
-  }
